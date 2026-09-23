2e0422586123ebaefdb2a47602c8b0023426b23b
Muhidin <muhidin280717@gmail.com>
2026-09-23 14:43:52 +0700

    feat: modyfy RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -59,7 +59,7 @@ class RegisterController extends Controller
         ]);
 
         return redirect()
-            ->route('login')
+            ->route('/')
             ->with('success', 'Registrasi berhasil, silahkan login dengan akun anda');
     }
 
